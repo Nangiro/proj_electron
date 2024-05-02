17a763d5ef9bf98b6d12f85a572d47ffa1c3f876
Daniel Abade <daniel.asa11@hotmail.com>
2024-05-01 21:09:57 -0300

    Integraçao back e bd

--- backend/src/configDB.js
+++ backend/src/configDB.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
+
 const db = new sqlite3.Database("database.db", (err)=>{
     if(err){
         console.log(err)
@@ -21,148 +22,52 @@ db.run(
         bairro TEXT,
         estado TEXT 
     )`,
-    (err) => {
+    (err) => {  
         if(err){
             console.log(err)
         } else {
-            console.log("TABLE CREATED WITH SUCCESS")
+            console.log("TABLE USERS CREATED WITH SUCCESS")
         }
     }
 )
 
-//Get all the data 
-const getAll = (callback) => {
-    db.all("SELECT id, username, cep, logradouro, numero, bairro, estado FROM users", (err, rows) => {
+db.run(
+    `CREATE TABLE IF NOT EXISTS session(
+        id INTEGER PRIMARY KEY,
+        token TEXT
+    )`,
+    (err) => {  
         if(err){
             console.log(err)
         } else {
-            callback(rows)
+            console.log("TABLE SESSION CREATED WITH SUCCESS")
         }
+    }
+)
+
+
+async function execute (query, params = null, callback) {
+    const res = db.all(query, params, function(err, result) {
+        console.log(callback)
+        if (err) callback(err, null);
+        callback(null, result);
     })
+
+    return res
 }
 
-const newUser = db.prepare(
-    `INSERT INTO users (username, password, cep, logradouro, numero, bairro, estado)
-    VALUES (?, ?, ?, ?, ?, ?,?)`,
-    (err) => {
-        if(err){
-            console.error(err);
-        } else {
-            console.log("The user was created")
-        }
-    }
-)
-
-const deleteUser = db.prepare(
-    `DELETE FROM users WHERE id === ?`,
-    (err)=>{
-        if(err){
-            console.error(err)
-        } else {
-            console.log("The user was deleted")
-        }
-    }
-)
-
-const editUser = db.prepare(
-    ` UPDATE users
-        SET username = ?,
-        SET password = ?,
-        SET cep = ?,
-        SET logradouro = ?,
-        SET numero = ?,
-        SET bairro = ?,
-        SET estado = ?,
-    `,
-    (err) => {
-        if(err){
-            console.error(err)
-        } else {
-            console.log("User was updated")
-        }
-    }
-)
-
-const server = http.createServer((req, res) => {
-    //Avoid CORS error
-    res.setHeader("Access-Control-Allow-Origin",  "*");
-    res.setHeader("Access-Control-Allow-Methods",  "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers",  "Content-Type");
-
-    //Return the info to the server
-    search((result) => {
-        res.write(JSON.stringify(result));
-        res.end();
+async function login(username, password, callback) {
+    const res = db.all('SELECT id, password, username FROM users WHERE username = ?;', [username], function(err, result) {
+        if (err) callback(err, null);
+        if (result.length >= 1) {
+            if (result[0].password === password) {
+                callback(null, result);
+            } else {
+                callback(null, "Erro");
+            }
+        } else callback(null, "Erro");
     })
+    return res
+}
 
-    //Check req types
-    if(req.method === "POST"){
-        let body = "";
-
-        //Get the info from server
-        req.on("data", (chunk) => {
-            body += chunk
-        })
-
-        req.on("end", () => {
-            //Deserialize the info
-            const parsedBody = JSON.parse(body)
-            console.log(parsedBody)
-            
-            newUser.run(
-                parsedBody.username,
-                parsedBody.password,
-                parsedBody.cep,
-                parsedBody.logradouro,
-                parsedBody.numero,
-                parsedBody.bairro,
-                parsedBody.estado
-            )
-            console.log("User successfully created")
-        })
-    } else if(req.method === "DELETE"){
-        let body = "";
-
-        //Get the info from server
-        req.on("data", (chunk) => {
-            body += chunk
-        })
-
-        req.on("end", () => {
-            //Deserialize the info
-            const parsedBody = JSON.parse(body)
-            console.log(parsedBody)
-
-            deleteUser.run(parsedBody.id)
-            console.log("User successfully deleted")
-        })
-    } else if(req.method === "PUT"){
-        let body = "";
-
-        //Get the info from server
-        req.on("data", (chunk) => {
-            body += chunk
-        })
-
-        req.on("end", () => {
-            //Deserialize the info
-            const parsedBody = JSON.parse(body)
-            console.log(parsedBody)
-            
-            editUser.run(
-                parsedBody.username,
-                parsedBody.password,
-                parsedBody.cep,
-                parsedBody.logradouro,
-                parsedBody.numero,
-                parsedBody.bairro,
-                parsedBody.estado
-            )
-            console.log("User successfully edited")
-        })
-    }
-})
-
-const port = 3000
-server.listen(port)
-console.log("It's on baby")
+module.exports = { execute, login }
